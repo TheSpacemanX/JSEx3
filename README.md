aef9b296d69afd429216c157417ebc4761d71b09
Chiozzi Emanuele <chiozzi.emanuele@osdbsesto>
2020-12-03 13:20:41 +0100

    add not optimized funciotns

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JSEx3
-Creazione di una classe javascript senza l'utilizzo del prototipo class: funzioni di addizione, sottrazione, moltiplicazione, divisione salvando risultati nella classe
+Creazione di una semplice calcolatrice che svolga le funzioni di somma, sottrazione, divisione e moltiplicazione. Si sviluppi in tre step:
+
+1. Pagina HTML con due caselle di testo e quattro pulsanti (operazioni)
+2. JQUERY assegnare funzioni di calcolo a tutti i pulsanti e stampare il risultato
+3. Mantenere in memoria il risultato tramite callback
--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-var add = function() {
+var operations = function() {
     $("#plus").on("click", function(){
-        console.log(parseInt($("#1st").val()+$("#2nd").val(),10));
+        $("#res").html($("#1st").val()*1+$("#2nd").val()*1);
+    });
+
+    $("#minus").on("click", function(){
+        $("#res").html($("#1st").val()*1-$("#2nd").val()*1);
+    });
+
+    $("#mult").on("click", function(){
+        $("#res").html($("#1st").val()*$("#2nd").val());
+    });
+
+    $("#div").on("click", function(){
+        $("#res").html($("#1st").val()/$("#2nd").val());
     });
 }
 
-document.addEventListener("DOMContentLoaded", add);
+document.addEventListener("DOMContentLoaded", operations);
