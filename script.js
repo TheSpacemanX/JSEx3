aa86860432f7b3dc4fd5a355e8021febc4ad7a97
Chiozzi Emanuele <chiozzi.emanuele@osdbsesto>
2020-12-03 13:40:29 +0100

    optimized

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 var operations = function() {
-    $("#plus").on("click", function(){
-        $("#res").html($("#1st").val()*1+$("#2nd").val()*1);
-    });
+    $("#plus").on("click", calc("+"));
+    $("#minus").on("click", calc("-"));
+    $("#mult").on("click", calc("*"));
+    $("#div").on("click", calc("/"));
 
-    $("#minus").on("click", function(){
-        $("#res").html($("#1st").val()*1-$("#2nd").val()*1);
-    });
-
-    $("#mult").on("click", function(){
-        $("#res").html($("#1st").val()*$("#2nd").val());
-    });
-
-    $("#div").on("click", function(){
-        $("#res").html($("#1st").val()/$("#2nd").val());
-    });
+    function calc(segno) {
+        var n1 = $("#1st").val()*1;
+        var n2 = $("#2nd").val()*1;
+        var res = 0;
+        switch(segno) {
+            case "+": res = n1+n2;
+            case "-": res = n1-n2;
+            case "*": res = n1*n2;
+            case "/": res = n1/n2;
+    };
+        $("#res").html(res);
+    }
 }
 
 document.addEventListener("DOMContentLoaded", operations);
